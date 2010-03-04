2071a2b16ad3c0b52dc0ed29ddc59b9ca9e2808c
galletly@zoo-samos <none@none>
2010-03-04 15:13:12 +0000

    Added TestWebDAVAccess.py; modified TestFileAccess.py

--- test/FileShare/tests/TestWebDAVAccess.py
+++ test/FileShare/tests/TestWebDAVAccess.py
@@ -1,28 +1,37 @@
-# $Id: TestFileAccess.py 1047 2009-01-15 14:48:58Z graham $
+# $Id: TestWebDAVAccess.py 1047 2009-01-15 14:48:58Z graham $
 #
 # Unit testing for FileAccess module
 #
 
 import os
+# Make sure python-kerberos package is installed
+import kerberos
 import sys
 import httplib
+import urllib2
+import urllib2_kerberos
+import re
+import base64
 import unittest
+from urlparse import urlparse
 
 sys.path.append("../..")
 
 readmetext="This directory is the root of the ADMIRAL shared file system.\n"
-mountpoint="mountadmiral"
+mountpoint="mountadmiralwebdav"
 readmefile="ADMIRAL.README"
+theurl="http://zakynthos.zoo.ox.ac.uk/webdav"
 
-class TestFileAccess(unittest.TestCase):
+class TestWebDAVAccess(unittest.TestCase):
 
     def setUp(self):
-        status=os.system('mount.cifs //zakynthos.zoo.ox.ac.uk/files '+mountpoint+' -o rw,user=test_admiral,password=test_admiral,nounix,forcedirectio')
+    # mount WebDAV share here
+        status=os.system('mount '+mountpoint)
         self.assertEqual(status, 0, 'Mount failure')
         return
 
     def tearDown(self):
-        os.system('umount.cifs '+mountpoint)
+        os.system('umount '+mountpoint)
         return
 
     # Test cases
@@ -44,18 +53,18 @@ class TestFileAccess(unittest.TestCase):
         return
 
     def testCreateFile(self):
-        f = open(mountpoint+'/testCreateFile.tmp','w+')
+        f = open(mountpoint+'/testCreateWebDAVFile.tmp','w+')
         assert (f), "File creation failed"
         f.write('Test creation of file\n')
         f.close()
-        f = open(mountpoint+'/testCreateFile.tmp','r')
+        f = open(mountpoint+'/testCreateWebDAVFile.tmp','r')
         l = f.readline()
         f.close()
         self.assertEqual(l, 'Test creation of file\n', 'Unexpected file content') 
         return
 
     def testUpdateFile(self):
-        filename = mountpoint+'/testUpdateFile.tmp'
+        filename = mountpoint+'/testUpdateWebDAVFile.tmp'
         f = open(filename,'w+')
         assert (f), "File creation failed"
         f.write('Test creation of file\n')
@@ -72,7 +81,7 @@ class TestFileAccess(unittest.TestCase):
         return
 
     def testRewriteFile(self):
-        filename = mountpoint+'/testRewriteFile.tmp'
+        filename = mountpoint+'/testRewriteWebDAVFile.tmp'
         f = open(filename,'w+')
         assert (f), "File creation failed"
         f.write('Test creation of file\n')
@@ -87,9 +96,9 @@ class TestFileAccess(unittest.TestCase):
         return
 
     def testDeleteFile(self):
-        filename1 = mountpoint+'/testCreateFile.tmp'
-        filename2 = mountpoint+'/testRewriteFile.tmp'
-        filename3 = mountpoint+'/testUpdateFile.tmp'
+        filename1 = mountpoint+'/testCreateWebDAVFile.tmp'
+        filename2 = mountpoint+'/testRewriteWebDAVFile.tmp'
+        filename3 = mountpoint+'/testUpdateWebDAVFile.tmp'
         # Test and delete first file
         try:
             s = os.stat(filename1)
@@ -132,9 +141,57 @@ class TestFileAccess(unittest.TestCase):
         authreq = str(res.status) + ' ' + res.reason
         print authreq
         self.assertEqual(authreq, '401 Authorization Required', 'Unexpected response') 
-        
         return
-         
+
+    def testWebDAVFileUrlLib(self):
+        #_ignore = kerberos.GSS_C_DELEG_FLAG
+        #from kerberos import GSS_C_DELEG_FLAG,GSS_C_MUTUAL_FLAG,GSS_C_SEQUENCE_FLAG
+        #_ignore, ctx = kerberos.authGSSClientInit('krbtgt/OX.AC.UK@OX.AC.UK', gssflags=GSS_C_DELEG_FLAG|GSS_C_MUTUAL_FLAG|GSS_C_SEQUENCE_FLAG)
+        _ignore, ctx = kerberos.authGSSClientInit('HTTP@zakynthos.zoo.ox.ac.uk')
+        _ignore = kerberos.authGSSClientStep(ctx, '')
+        tgt = kerberos.authGSSClientResponse(ctx)
+        opener = urllib2.build_opener()
+        opener.add_handler(urllib2_kerberos.HTTPKerberosAuthHandler())
+        resp = opener.open(theurl)
+        print resp
+
+        return
+
+        req = urllib2.Request(theurl)
+        try:
+           handle = urllib2.urlopen(req)
+        except IOError, e:
+           pass
+        else:
+           assert (False), theurl + " isn't protected by authentication."
+
+        if not hasattr(e, 'code') or e.code != 401:
+           # we got an error - but not a 401 error
+           assert (False), theurl + " Error: " + e
+
+        authline = e.headers['www-authenticate']
+        # this gets the www-authenticate line from the headers
+        # which has the authentication scheme and realm in it
+
+        authobj = re.compile(
+           r'''(?:\s*www-authenticate\s*:)?\s*(\w*)\s+realm=['"]([^'"]+)['"]''',
+           re.IGNORECASE)
+      # this regular expression is used to extract scheme and realm
+        matchobj = authobj.match(authline)
+
+        if not matchobj:
+           # if the authline isn't matched by the regular expression
+           # then something is wrong
+           assert (False), "Malformed authentication header: " + authline
+
+        scheme = matchobj.group(1)
+        realm = matchobj.group(2)
+       # here we've extracted the scheme
+       # and the realm from the header
+        print scheme
+        print realm
+
+        return
 
     # Sentinel/placeholder tests
 
@@ -177,16 +234,17 @@ def getTestSuite(select="unit"):
             , "testRewriteFile"
             , "testUpdateFile"
             , "testDeleteFile"
-            , "testWebDAVFile"
             ],
         "integration":
             [ "testIntegration"
             ],
         "pending":
             [ "testPending"
+            , "testWebDAVFile"
+            , "testWebDAVFileUrlLib"
             ]
         }
-    return TestUtils.getTestSuite(TestFileAccess, testdict, select=select)
+    return TestUtils.getTestSuite(TestWebDAVAccess, testdict, select=select)
 
 # Run unit tests directly from command line
 if __name__ == "__main__":
