31556b0d8817f2a77eb76797ab62f00f59f60a94
diana.galletly@gmail.com <none@none>
2010-07-07 12:34:59 +0100

    Changed test scripts to use new Databank URIs.

--- spike/uploadnewapps.sh
+++ spike/uploadnewapps.sh
@@ -5,6 +5,6 @@
 
 rm apps.zip
 zip -r apps.zip apps
-#curl http://163.1.127.173/packages/admiral-test -F filename=apps.zip -F file=@apps.zip -F id=apps -u admiral:admiral -H "Accept: text/html,application/xhtml+xml"
-curl http://localhost:9080/packages/admiral-test  -F "file=@apps.zip;filename=apps.zip;type=application/zip" -F id=apps -u admiral:admiral -H "Accept: text/html,application/xhtml+xml"
+curl http://163.1.127.173/admiral-test/packages -F filename=apps.zip -F file=@apps.zip -F id=apps -u admiral:admiral -H "Accept: text/html,application/xhtml+xml"
+#curl http://localhost:9080/packages/admiral-test  -F "file=@apps.zip;filename=apps.zip;type=application/zip" -F id=apps -u admiral:admiral -H "Accept: text/html,application/xhtml+xml"
 
