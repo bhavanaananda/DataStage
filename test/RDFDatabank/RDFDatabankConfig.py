7eff6859dfefdc20762ab7c3126bd001a0175829
graham <none@none>
2010-07-26 14:42:38 +0100

    Refactored test to use separate RDFdatabank configuration module, and added initial test data for metadata merging test

---
+++ test/RDFDatabank/RDFDatabankConfig.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# $Id: $
+"""
+Define configuration for RDFDatabank testing
+
+$Rev: $
+"""
+
+class RDFDatabankConfig:
+
+    # Access via SSH tunnel
+    endpointhost="localhost:9080"
+    endpointpath="/admiral-test/packages/"
+
+    # Access via IP address
+    endpointhost="163.1.127.173"
+    endpointpath="/admiral-test/"
+
+    # Access credentials for testing
+    endpointuser="admiral"
+    endpointpass="admiral"
+
+    # Later, may define methods to override these defaults, e.g. from a configuration file
+
+# End.
