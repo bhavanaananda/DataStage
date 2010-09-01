15518b0f55092359b1cbb4682d85e4dd33cf9db7
graham <none@none>
2010-09-01 13:54:35 +0100

    Tweak sample script for creating test users Create dummy /mnt/lv-admiral-data/home path when building test system

--- test/FileShare/tests/CreateTestUsers.sh
+++ test/FileShare/tests/CreateTestUsers.sh
@@ -3,9 +3,9 @@
 # Sample script for creating test users
 # (see test/FileShare/tests/TestConfig.py)
 
-admiraluseradd TestUser1  "Test user 1"       RGMember       room1 111111 user1
-admiraluseradd TestUser2  "Test user 2"       RGMember       room2 222222 user2
-admiraluseradd TestLeader "Test group leader" RGLeader       room3 333333 leader
-admiraluseradd TestCollab "Test collaborator" RGCollaborator room4 444444 collab
+./admiraluseradd.sh TestUser1  "Test user 1"       RGMember       room1 111111 user1
+./admiraluseradd.sh TestUser2  "Test user 2"       RGMember       room2 222222 user2
+./admiraluseradd.sh TestLeader "Test group leader" RGLeader       room3 333333 leader
+./admiraluseradd.sh TestCollab "Test collaborator" RGCollaborator room4 444444 collab
 
 # End.
