048f2ed982ed829253e9fa064d7848085185e5ed
bweaver <none@none>
2010-09-08 11:36:48 +0100

    calls to enable population of derivedFrom and lastModified fields in dataset status table

--- spike/apps/RDFManifest.js
+++ spike/apps/RDFManifest.js
@@ -20,70 +20,6 @@ if (typeof RDFManifest == "undefined")
     RDFManifest = {};
 }
 
-RDFManifest.myFunction = function(jelem, fileAbsolutePaths)
-{
-	
-	/*
-	tree creation routine
-	basic idea:
-	if term is new at level n, create it
-	if term is not new at level n, 
-		create it if entire path leading to it and including it ( = 'prior path') is unique
-		* 	for example: given apps/x/y/z    y has prior path =jjv apps/x/y  
-		the whole path must be checked
-	an array of paths must be created for each level, including the term at that level
-	if an array of paths does not yet exist for a level 
-		1. create it
-		2. push it onto the array of arrays, in which it will occupy a position equal to its level
-			example: if there are a maximum of 8 arrays for a data set, the arrayOfArrays will eventually have 8 members, each of which records all prior paths that have so far been created for that level 
-	for each term,
-		the array of paths must be searched
-	
-	
-	*/
-	
-	/*
-	 * simpler way
-	 * for each absolutePath
-	 * split
-	 * process each segment
-	 * 	for each segment
-	 * 		reconstruct the inclusive path
-	 * 		if inclusivePath not already in pathsCreated array
-	 * 			create
-	 * 		else do not create
-	 */
-
-
-	var inclusivePathsAlreadyCreated = new Array();
-	
-	for(i = 0; i < fileAbsolutePaths.length; i++){
-//		jelem.append("<br />" + fileAbsolutePaths[i]); 
-		var pathSegments = fileAbsolutePaths[i].split("/");
-		var previousLevel = "";
-		var currentLevel = "";
-		//process each segment
-		for (j = 0;j < pathSegments.length; j++){
-			//reconstruct inclusive path for each successive term
-			if (j < pathSegments.length-1) {
-				currentLevel += pathSegments[j];
-				currentLevel += "/";
-			}
-			else {
-				currentLevel += pathSegments[j];
-			}
-			//check to see if this inclusivePath has already been created
-			if (! inclusivePathsAlreadyCreated.contains(currentLevel)) {
-				//create
-				//add the node to previousLevel (j - 1)
-				RDFManifest.addNode(pathSegments[j]);
-			}
-			previousLevel += pathSegments[j];
-			
-		}
-	}
-
-}
 
 
 	
@@ -140,32 +76,20 @@ RDFManifest.display = function (jelem)
     m.eval(function (rq, callback)
     {
         rq = rq.where('?s ?p ?o');
-//        jelem.append('div')
-//         .html("Manifest contains "+rq.length+" triples (<a href=\""+rq[0].s.value+"/manifest.rdf\">show manifest</a>");
-        jelem.html("<p>Manifest contains "+rq.length+" triples (<a href=\""+rq[0].s.value+"/manifest.rdf\">show manifest</a>)</p>");
-/*        jelem.append("<div style=\"display: none;\">");
-        jelem.append("<table");
-        jelem.append("<tr style=\"display: none;\"><td><strong>Subject</strong></td><td><strong>Property</strong></td><td><strong>Object</strong></td></tr>");
-
         rq.each(function ()
         {
-            jelem.append("<tr><td>"+this.s.value+"</td><td>"+this.p.value+"</td><td>"+this.o.value+"</td></tr>"); 
-        });
-        jelem.append("</table>");
-        jelem.append("</div>");*/
-/*        while (i<rq.length) {
-            jelem.append("<p>"+rq.eq(i).dump({format:'application/json', serialize: true})+"</p>");
-            i++;
-        }*/
-        rq.each(function ()
-        {
+            jelem.text("");
             if(this.p.value.toString()=="http://purl.org/dc/terms/isVersionOf") {
-                jelem.append("<p>Dataset derived from " + this.o.value + "</p>");
+//                jelem.append("<p>Dataset derived from " + this.o.value + "</p>");
+                  jQuery("#derivedFrom > a").text(this.o.value.toString());
+                  jQuery("#derivedFrom > a").attr("href", this.o.value);
             } else if(this.p.value.toString()=="http://purl.org/dc/terms/modified") {
-                jelem.append("<p>Dataset last modified at " + this.o.value + "</p>");
+                jQuery("#lastModified").text(this.o.value);
+ 
             }
         });
-        jelem.append("<h3>Contents of Dataset " + "<br /><br />" + "<a href=\""+rq[0].s.value+"\">" + rq[0].s.value + "</a></h3>");
+        jelem.append("<h3>Contents of Dataset </h3>");
+        jQuery("#datasetLink").attr("href", rq[0].s.value);
 
         var newText = "";
         var fileAbsolutePaths = new Array();
@@ -174,20 +98,15 @@ RDFManifest.display = function (jelem)
         rq.each(function ()
         {
              if(shuffl.starts(this.s.value.toString(), this.o.value.toString())) {
-//               jelem.append("<tr><td>"+this.s.value+"</td><td><a href=\""+this.o.value+"\">"+jQuery.uri.relative(this.o.value, this.s.value).toString()+"</a></td></tr>");
-//				 jelem.append("<a href=\""+this.o.value+"\">"+jQuery.uri.relative(this.o.value, this.s.value).toString()+"</a>");
-//				 jelem.append("\n");
 				 newText = newText + "<a href=\""+this.o.value+"\">"+jQuery.uri.relative(this.o.value, this.s.value).toString()+"</a><br />";
 				 
 				 fileAbsolutePaths.push(jQuery.uri.relative(this.o.value, this.s.value).toString());
             }
         });
-    //    jelem.append("</table>");
-    	 
-    	  jelem.append("<div style=\"border-color:#600;border-width: 1px 1px 1px 1px;border-style:solid;width:700px;height:100px;background-color: #FFE;overflow:auto;\" >" + newText + "</div>");
  
+    	jelem.append("<div style=\"border-color:#600;border-width: 1px 1px 1px 1px;border-style:solid;width:700px;height:100px;background-color: #FFE;overflow:auto;\" >" + newText + "</div>");
 
-		  RDFManifest.myFunction(jelem, fileAbsolutePaths);
+
              
  
     	 
@@ -203,6 +122,6 @@ RDFManifest.display = function (jelem)
     	 
     });
 
-    m.exec("http://163.1.127.173/admiral-test/datasets/apps", shuffl.noop);
+    m.exec("/admiral-test/datasets/apps", shuffl.noop);
 };
 
