a8fbfb4014f36c5f99cdb0dc3c0b3ce4e5316b2d
bhavana <none@none>
2010-10-20 11:57:22 +0100

    Test file operations in user private area.

---
+++ test/FileShare/tests/TestFilePrivateArea.py
@@ -0,0 +1,224 @@
+# $Id: TestFilePrivateArea.py 1047 2009-01-15 14:48:58Z graham $
+#
+# Unit testing for access to private file area
+#
+
+import os
+import sys
+import httplib
+import urllib2
+import unittest
+import subprocess
+
+sys.path.append("../..")
+
+from TestConfig import TestConfig
+
+import TestHttpUtils
+
+class TestFilePrivateArea(unittest.TestCase):
+        
+    def setUp(self):
+        return
+
+    def tearDown(self):
+        #TestHttpUtils.do_cifsUnmount()
+        return
+
+    def HTTP_redirect(self, opener, method, uri, data, content_type):
+        TestHttpUtils.do_HTTP_redirect(opener, method, uri, data, content_type)
+        return
+    
+    def cifsMount(self, areaName, userName, userPass):
+        status= TestHttpUtils.do_cifsMount(areaName, userName, userPass)
+        self.assertEqual(status, 0, 'CIFS Mount failure')
+        return
+
+    def cifsUnmount(self):
+        TestHttpUtils.do_cifsUnmount()
+        return
+
+    def cifsCreateFile(self, fileName, fileContent):
+        TestHttpUtils.do_cifsCreateFile(fileName, fileContent)
+        return
+
+    def cifsReadFile(self, fileName , fileContent):
+        l = TestHttpUtils.do_cifsReadFile(fileName , fileContent)
+        return  
+    
+    def cifsUpdateFile(self,fileName, fileUpdateContent):
+        TestHttpUtils.do_cifsUpdateFile(fileName, fileUpdateContent)
+        return
+    
+    def cifsDeleteFile(self,fileName):
+        TestHttpUtils.do_cifsDeleteFile(fileName)
+        return   
+
+    def httpAuthenticationHandler(self,userName, userPass):
+        authhandler = TestHttpUtils.do_httpAuthenticationHandler(userName, userPass)
+        return authhandler
+    
+    def httpCreateFile(self, userName, userPass, fileName, fileContent):
+        TestHttpUtils.do_httpCreateFile('private/'+username, userName, userPass, fileName, fileContent)
+        return
+
+    def httpReadFile(self, areaName, userName, userPass,fileName, fileContent):
+        thepage = TestHttpUtils.do_httpReadFile( areaName, userName, userPass,fileName, fileContent)
+        self.assertEqual(thepage,fileContent)
+        return
+    
+    def httpUpdateFile(self,areaName, userName, userPass,fileName, fileUpdateContent):
+        TestHttpUtils.do_httpUpdateFile(areaName, userName, userPass,fileName, fileUpdateContent)
+        return
+    
+    def httpDeleteFile(self,areaName, userName, userPass,fileName):
+        TestHttpUtils.do_httpDeleteFile( areaName, userName, userPass,fileName)
+        return
+    
+
+    # Test cases
+
+    def testNull(self):
+        assert (True), "True expected"
+        return
+
+    def testUserACreateCIFSUserAReadCIFS(self): 
+        fileName = 'testCreateFileCIFS.tmp'
+        fileContent= 'Test creation of file\n'
+        self.cifsMount(TestConfig.userAname, TestConfig.userAname, TestConfig.userApass)
+        self.cifsCreateFile(fileName, fileContent)       
+        self.cifsReadFile(fileName, fileContent) 
+        self.cifsUnmount()
+        return
+
+    def testUserACreateCIFSUserAReadHTTP(self): 
+        fileName = 'testCreateFileCIFS.tmp'
+        fileContent= 'Test creation of file\n'
+        self.cifsMount(TestConfig.userAname, TestConfig.userAname, TestConfig.userApass)
+        self.cifsCreateFile(fileName, fileContent)       
+        self.httpReadFile(TestConfig.userAname, TestConfig.userAname, TestConfig.userApass,fileName, fileContent) 
+        self.cifsUnmount()
+        return
+    
+    def testUserAUpdateCIFSUserAReadCIFS(self): 
+        fileName = 'testCreateFileCIFS.tmp'
+        fileContent= 'Test creation of file\n'
+        fileUpdateContent= 'Test update of file\n'
+        self.cifsMount(TestConfig.userAname, TestConfig.userAname, TestConfig.userApass)
+        self.cifsCreateFile(fileName, fileContent)       
+        self.cifsUpdateFile(fileName, fileUpdateContent)
+        updatedFileContent= fileContent + fileUpdateContent
+        self.cifsReadFile(fileName, updatedFileContent) 
+        self.cifsUnmount()
+        return
+    
+    def testUserAUpdateHTTPUserAReadCIFS(self): 
+        fileName = 'testCreateFileCIFS.tmp'
+        fileContent= 'Test creation of file\n'
+        fileUpdateContent= 'Test update of file\n'
+        self.cifsMount(TestConfig.userAname, TestConfig.userAname, TestConfig.userApass)
+        self.cifsCreateFile(fileName, fileContent)       
+        updatedFileContent= fileContent + fileUpdateContent
+        # HTTP Update overwrites(does not append the original) the file, hence expecting the updated content when read again.
+        self.httpUpdateFile(TestConfig.userAname, TestConfig.userAname, TestConfig.userApass,fileName,updatedFileContent)
+        self.cifsReadFile(fileName, updatedFileContent) 
+        self.cifsUnmount()
+        return
+    
+    def testUserACreateCIFSUserADeleteCIFS(self): 
+        fileName = 'testCreateFileCIFS.tmp'
+        fileContent= 'Test creation of file\n'
+        self.cifsMount(TestConfig.userAname, TestConfig.userAname, TestConfig.userApass)
+        self.cifsCreateFile(fileName, fileContent)     
+        self.cifsDeleteFile(fileName)
+        self.cifsUnmount()
+        return
+    
+    def testUserACreateCIFSUserADeleteHTTP(self): 
+        fileName = 'testCreateFileCIFS.tmp'
+        fileContent= 'Test creation of file\n'
+        self.cifsMount(TestConfig.userAname, TestConfig.userAname, TestConfig.userApass)
+        self.cifsCreateFile(fileName, fileContent)       
+        self.httpDeleteFile(TestConfig.userAname, TestConfig.userAname, TestConfig.userApass,fileName)
+        self.cifsUnmount()
+        return
+
+    def testUserACreateHTTPUserAReadHTTP(self): 
+        fileName = 'testCreateFileCIFS.tmp'
+        fileContent= 'Test creation of file\n'
+        self.httpCreateFile(TestConfig.userAname, TestConfig.userApass, fileName, fileContent) 
+        self.httpReadFile(TestConfig.userAname, TestConfig.userAname, TestConfig.userApass,fileName, fileContent) 
+        return
+  
+    #def testUserACreateCIFSUserAReadHTTP(self): 
+       
+    #def testUserAUpdateCIFSUserAReadCIFS(self): 
+    
+    #def testUserAUpdateHTTPUserAReadCIFS(self): 
+       
+    #def testUserACreateCIFSUserADeleteCIFS(self): 
+        
+    #def testUserACreateCIFSUserADeleteHTTP(self): 
+        
+
+
+    # Sentinel/placeholder tests
+
+    def testUnits(self):
+        assert (True)
+
+    def testComponents(self):
+        assert (True)
+
+    def testIntegration(self):
+        assert (True)
+
+    def testPending(self):
+        assert (False), "No pending test"
+
+# Assemble test suite
+
+from MiscLib import TestUtils
+
+def getTestSuite(select="unit"):
+    """
+    Get test suite
+
+    select  is one of the following:
+            "unit"      return suite of unit tests only
+            "component" return suite of unit and component tests
+            "all"       return suite of unit, component and integration tests
+            "pending"   return suite of pending tests
+            name        a single named test to be run
+    """
+    testdict = {
+        "unit": 
+            [ "testUnits"
+            , "testNull"
+            , "testUserACreateCIFSUserAReadCIFS"
+            , "testUserACreateCIFSUserAReadHTTP"
+            , "testUserAUpdateCIFSUserAReadCIFS"
+            , "testUserAUpdateHTTPUserAReadCIFS"
+            , "testUserACreateCIFSUserADeleteCIFS"
+            , "testUserACreateCIFSUserADeleteHTTP"
+            #, "testUserACreateHTTPUserAReadHTTP"
+            ],
+        "component":
+            [ "testComponents"
+            ],
+        "integration":
+            [ "testIntegration"
+            ],
+        "pending":
+            [ "testPending"
+            ]
+        }
+    return TestUtils.getTestSuite(TestFilePrivateArea, testdict, select=select)
+
+# Run unit tests directly from command line
+if __name__ == "__main__":
+    TestUtils.runTests("TestFilePrivateArea.log", getTestSuite, sys.argv)
+
+# End.
+
+
--- test/FileShare/tests/TestHttpUtils.py
+++ test/FileShare/tests/TestHttpUtils.py
@@ -8,6 +8,7 @@ import sys
 import httplib
 import urllib2
 
+from TestConfig import TestConfig
 
 # Execute a specified HTTP method using a supplied urllib2 opener object,
 # following a single HTTP 301 redirection response
@@ -27,3 +28,99 @@ def do_HTTP_redirect(opener, method, uri, data, content_type):
         else:
             raise e     # propagate exception
     return
+
+def do_cifsMount(areaName, userName, userPass):
+    #print "do_cifsMount: "+areaName+", "+userName+", "+userPass+", "+TestConfig.hostname
+    mountcommand = ( 'mount.cifs //%(host)s/%(share)s/private/%(area)s %(mountpt)s -o rw,user=%(user)s,password=%(pass)s,nounix,forcedirectio' %
+                     { 'host': TestConfig.hostname
+                     , 'share': TestConfig.cifssharename
+                     , 'area': areaName
+                     , 'user': userName
+                     , 'mountpt': TestConfig.cifsmountpoint
+                     , 'pass': userPass
+                     } )
+    #print mountcommand
+    status=os.system(mountcommand)
+    return status
+
+def do_cifsUnmount():
+    os.system('umount.cifs ' + TestConfig.cifsmountpoint)
+
+def do_cifsCreateFile(fileName, fileContent):
+    ###print "do_cifsCreateFile: "+TestConfig.cifsmountpoint + '/' + fileName
+    f = open(TestConfig.cifsmountpoint + '/' + fileName, 'w+')
+    assert f, "File creation failed"
+    f.write(fileContent)
+    f.close()
+    return 
+
+def do_cifsReadFile(fileName , fileContent):
+    f = open(TestConfig.cifsmountpoint + '/' + fileName, 'r')
+    l = f.read()
+    f.close()
+    return l
+
+def do_cifsUpdateFile(fileName, fileUpdateContent):
+    f = open(TestConfig.cifsmountpoint + '/' + fileName,'a+')
+    f.write(fileUpdateContent)
+    f.close()
+    return
+
+def do_cifsDeleteFile(fileName):
+    # Test and delete file
+    try:
+        s = os.stat(TestConfig.cifsmountpoint + '/'+ fileName)
+    except:
+        assert (False), "File "+ fileName+" not found or other stat error"
+    os.remove(TestConfig.cifsmountpoint + '/'+ fileName)
+    try:
+        s = os.stat(TestConfig.cifsmountpoint + '/'+ fileName)
+        assert (False), "File "+ fileName+" not deleted"
+    except:
+        pass
+    return
+
+
+def do_httpAuthenticationHandler(userName, userPass):
+    passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
+    passman.add_password(None, TestConfig.webdavbaseurl, userName, userPass)
+    authhandler = urllib2.HTTPBasicAuthHandler(passman)
+    return authhandler
+
+def do_httpCreateFile(areaName, userName, userPass, fileName, fileContent):
+    authhandler = do_httpAuthenticationHandler(userName, userPass)
+    opener = urllib2.build_opener(authhandler)
+    urllib2.install_opener(opener)      
+    # Write data to server
+    do_HTTP_redirect(opener, "PUT",
+        TestConfig.webdavbaseurl + '/' + areaName + '/' + fileName, 
+        fileContent, 'text/plain')
+    return 
+
+def do_httpReadFile(areaName, userName, userPass,fileName, fileContent):
+    authhandler = do_httpAuthenticationHandler(userName, userPass)
+    opener = urllib2.build_opener(authhandler)
+    urllib2.install_opener(opener)       
+    phan=urllib2.urlopen(TestConfig.webdavbaseurl+'/private/'+ userName +'/' + fileName)
+    thepage=phan.read()
+    return thepage
+
+def do_httpUpdateFile(areaName, userName, userPass,fileName, fileUpdateContent):
+    authhandler = do_httpAuthenticationHandler(userName, userPass)
+    opener = urllib2.build_opener(authhandler)
+    urllib2.install_opener(opener)      
+    # Write/update data to server
+    do_HTTP_redirect(opener, "PUT",
+        TestConfig.webdavbaseurl+'/private/'+ userName +'/' + fileName, 
+        fileUpdateContent, 'text/plain')
+    return
+
+def do_httpDeleteFile(areaName, userName, userPass,fileName):
+    authhandler = do_httpAuthenticationHandler(userName, userPass)
+    opener = urllib2.build_opener(authhandler)
+    do_HTTP_redirect(opener, "DELETE", 
+        TestConfig.webdavbaseurl+'/private/'+ userName +'/' + fileName,
+        None, None)
+    return
+    
+
