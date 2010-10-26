9d568ad63819c7f63ad86f95e7d2ffb8b81fc8aa
bhavana <none@none>
2010-10-26 13:55:45 +0100

    Add configuration for Development Group

---
+++ admiral-base/zoo-admiral-devel/hostconfig.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Configuration parameters for host zoo-admiral-devel
+#
+# (See also: admiralconfig.sh)
+#
+
+# Samba/Windows workgroup name for research group files
+WORKGROUP="EvolutionaryDevelopment"
+
+# Username initially designated as research group leader
+# (System scripts will attempt to ensure that there is always a user 
+# who is the designated research group leader)
+LEADERNAME="SebShimeld"
+
+# End.
+
