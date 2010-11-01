6b173d7f65fe51d7c1df6eef1a3ce035fd7b1bde
bhavana <none@none>
2010-11-01 14:29:50 +0000

    CGI spike code
    --HG--
    rename : spike/applications/Admiral/ViewOrSubmitDatasets/cgi-bin/cgi.py => spike/applications/Admiral/ViewOrSubmitDatasets/cgi-bin/examplecgi.py

--- spike/applications/Admiral/ViewOrSubmitDatasets/cgi-bin/examplecgi.py
+++ spike/applications/Admiral/ViewOrSubmitDatasets/cgi-bin/examplecgi.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python
+#from __future__ import absolute_import
+
 import cgi
 
 def main():
     print "Content-type:text/html\n"
+    print sys.path
+    #print "<h1> Error! Please enter your first name!</h1>"
     form = cgi.FieldStorage() #parse the query
-    if form.has_key("firstname" and form["firstname"].value!= "" ):
+    if form.has_key("firstname") and form["firstname"].value!= "" :
         print "<h1>Hello" , form["firstname"].value, "</h1>"
     else :
-        print "<h1> Error! Please enter your first name!/h1>"
-    
+        print "<h1> Error! Please enter your first name!</h1>"
+   
     #try:
     #except:
     #cgi.print_exception()
     
     
     
-    main()
+main()
     
