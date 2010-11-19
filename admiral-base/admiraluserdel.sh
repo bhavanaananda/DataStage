3495302d6e0701d3c3a72052a2fedea04f5e26d6
graham <none@none>
2010-11-19 16:01:24 +0000

    Update ADMIRAL user creation and deletion scripts

--- admiral-base/admiraluserdel.sh
+++ admiral-base/admiraluserdel.sh
@@ -3,7 +3,7 @@
 source admiralconfig.sh
 
 if [[ "$1" == "" ]]; then
-  echo "Usage: $0 username"
+  echo "Usage: $0 username [purge]"
   exit 1
 elif [[ "$1" == "$RGLeaderName" ]]; then
   echo "Do not use this script to delete the Research Group Leader!"
@@ -11,12 +11,38 @@ elif [[ "$1" == "$RGLeaderName" ]]; then
   echo "Then run the 'admiralleaderchange.sh' script."
   exit 1
 else 
-  chown -R $RGLeaderName:RGLeader /home/data/private/$1
-  chown -R $RGLeaderName:RGLeader /home/data/shared/$1
-  chown -R $RGLeaderName:RGLeader /home/data/collab/$1
-  chown -R $RGLeaderName:RGLeader /mnt/lv-admiral-data/home/$1
+
+  if [[ "$2" == "purge" ]]; then
+
+    rm -rf "/home/data/private/$1"
+    rm -rf "/home/data/shared/$1"
+    rm -rf "/home/data/collab/$1"
+    rm -rf "/mnt/lv-admiral-data/home/$1"
+    rm -rf "/home/$1"
+    rm -rf "/home/$1-saved"
+    rm -rf "/home/$1-deleted"
+
+  else
+
+    chown -R $RGLeaderName:RGLeader /home/data/private/$1
+    chown -R $RGLeaderName:RGLeader /home/data/shared/$1
+    chown -R $RGLeaderName:RGLeader /home/data/collab/$1
+    chown -R $RGLeaderName:RGLeader /mnt/lv-admiral-data/home/$1
+    if [[ -L "/home/$1" ]]; then
+      rm "/home/$1"
+    elif [[ -e "/home/$1" ]]; then
+      mv "/home/$1" "/home/$1-deleted"
+    fi
+    if [[ -e "/home/$1-saved" ]]; then
+      mv /home/$1-saved /home/$1
+    fi
+
+  fi
+
   smbldap-userdel -r $1
-  rm -rf /home/$1-saved
   rm /etc/apache2/conf.d/user.$1
   rm /root/admiralresearchgroupmembers/$1.sh
+
 fi
+
+# End.
