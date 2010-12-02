9bf9d1dc73356a395296fafd9029af0dfbf2b6d9
bhavana <none@none>
2010-12-02 15:22:54 +0000

    Added an integrated test suite for DatasetSubmissio Tool

---
+++ src/SubmitDatasetHandler/tests/TestAll.py
@@ -0,0 +1,39 @@
+# $Id: TestAll.py 1047 2009-01-15 14:48:58Z graham $
+#
+# Unit testing for WebBrick library functions (Functions.py)
+# See http://pyunit.sourceforge.net/pyunit.html
+#
+
+import sys, unittest, logging
+
+# Add main library directory to python path
+sys.path.append("../..")
+
+import TestSubmitDataset
+import TestSubmitDatasetHandler
+import TestDirectoryListingHandler
+import TestMetadataMerging
+import TestGetMetadata
+
+# Code to run unit tests from all library test modules
+def getTestSuite(select="all"):
+    suite = unittest.TestSuite()
+
+    suite.addTest(TestSubmitDataset.getTestSuite(select=select))
+    suite.addTest(TestSubmitDatasetHandler.getTestSuite(select=select))
+    suite.addTest(TestDirectoryListingHandler.getTestSuite(select=select))
+    suite.addTest(TestMetadataMerging.getTestSuite(select=select))
+    suite.addTest(TestGetMetadata.getTestSuite(select=select))
+    return suite
+
+from MiscLib import TestUtils
+
+if __name__ == "__main__":
+    print "============================================================"
+    print "This test suite needs to run under a Linux operating system"
+    print "Edit TestConfig.py to specify hostname and other parameters"
+    print "Create test accounts on target system to match TestConfig.py"
+    print "============================================================"
+    TestUtils.runTests("TestAll", getTestSuite, sys.argv)
+
+# End.
