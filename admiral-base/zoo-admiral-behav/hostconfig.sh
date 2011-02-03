4e5dfd25785b120e337dc69d3f9390146867984c
bhavana <none@none>
2011-02-03 11:04:09 +0000

    Add configuration for Behaviour Group

---
+++ admiral-base/zoo-admiral-behav/hostconfig.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Configuration parameters for host zoo-admiral-behav
+#
+# (See also: admiralconfig.sh)
+#
+
+# Samba/Windows workgroup name for research group files
+WORKGROUP="SHOTTON"
+
+# Username initially designated as research group leader
+# (System scripts will attempt to ensure that there is always a user 
+# who is the designated research group leader)
+LEADERNAME="AlexKacelnik"
+
+# Databank host and silo names
+
+DATABANKHOST="databank.ora.ox.ac.uk"
+DATABANKSILO="admiral-test"
+
+# End.
