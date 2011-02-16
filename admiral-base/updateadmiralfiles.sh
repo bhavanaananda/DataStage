652c19255b18835704b44bd23eaa74359f394f17
bhavana <none@none>
2011-02-16 13:43:18 +0000

    Updated  udapteadmiralfile to include  apache-databank-proxy file that includes the new static inclusion.

--- admiral-base/updateadmiralfiles.sh
+++ admiral-base/updateadmiralfiles.sh
@@ -66,7 +66,8 @@ migrate sshd_config                /etc/ssh
 migrate apache2.conf               /etc/apache2
 migrate apache-default             /etc/apache2/sites-available/default
 migrate apache-default-ssl         /etc/apache2/sites-available/default-ssl
-migrate apache-databank-proxy      /etc/apache2/sites-available/apache-databank-proxy
+migrate apache-databank-proxy      /etc/apache2/sites-available/databank-proxy
+migrate apache-databank-proxy      /etc/apache2/sites-enabled/databank-proxy
 ### migrate webauth.conf               /etc/apache2/webauth
 ### migrate webauth.keytab             /etc/apache2/webauth
 ### migrate webdav.keytab              /etc/apache2
