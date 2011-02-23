0c931785c513e84c105ddc9d1bd47cf711b5dd50
bhavana <none@none>
2011-02-23 12:42:31 +0000

    (some more files) Removed setups and references to RGLeader - Zakynthos & general base files

--- admiral-base/admiralconfig.sh
+++ admiral-base/admiralconfig.sh
@@ -3,8 +3,6 @@
 # ADMIRAL system parameters
 # (see also apache-default and apache-default-ssl)
 
-source /root/admiralconfig.d/admiralRGLeader.sh
-
 RGLeaderGID=600
 RGMemberGID=601
 RGCollabGID=602
@@ -13,6 +11,5 @@ RGOrphanGID=603
 RESEARCHGROUPNAME="%{RESEARCHGROUPNAME}"
 ADMIRALHOSTNAME="%{HOSTNAME}"
 SAMBAWORKGROUP="%{WORKGROUP}"
-
 DATABANKHOST="%{DATABANKHOST}"
 DATABANKSILO="%{DATABANKSILO}"
