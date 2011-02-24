3961c485906016458968d3ff94b0c073042ad35c
graham <none@none>
2011-02-24 10:31:33 +0000

    Add new configuration for ADMIRAL demonstration system

---
+++ admiral-base/admiral-demo/hostconfig.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Configuration parameters for host zoo-admiral-devel
+#
+# (See also: admiralconfig.sh)
+#
+
+RESEARCHGROUPNAME="ADMIRAL Demonstrator"
+
+ADMIRALHOSTNAME="admiral-demo"
+
+# Samba/Windows workgroup name for research group files
+WORKGROUP="ADMIRAL"
+
+# Databank host and silo names
+
+DATABANKHOST="databank-demo"
+DATABANKSILO="admiral-demo"
+
+# End.
+
