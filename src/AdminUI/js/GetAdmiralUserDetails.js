45b06d3634701d69edaf4b5e7e3362b22969faa9
bhavana <none@none>
2011-04-10 20:53:21 +0100

    (Work in Progress) Admiral user Interface.

---
+++ src/AdminUI/js/GetAdmiralUserDetails.js
@@ -0,0 +1,55 @@
+/**
+ * @fileoverview
+ * Get ADMIRAL user details
+ * 
+ * 
+ * @author Bhavana Ananda
+ * @version $Id: $
+ * 
+ * Coypyright (C) 2010, University of Oxford
+ *
+ * Licensed under the MIT License.  You may obtain a copy of the License at:
+ *
+ *     http://www.opensource.org/licenses/mit-license.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Get ADMIRAL user details
+ */
+ 
+ 
+if (typeof admiral == "undefined")
+{
+    admiral = {};
+}
+
+admiral.userDetails = function (userID)
+{ 
+    urlval = "http://admiral/user/"+userID;
+    jQuery.ajax({
+                    type:         "GET",
+                    url:           urlval,
+                    dataType:     "json",
+                    beforeSend:   function (xhr)
+                        {
+                            xhr.setRequestHeader("Accept", "application/JSON");
+                        },
+                    success:      function (data, status, xhr)
+                        {   
+                            //log.debug("Get ADMIRAL user details: " + jQuery.toJSON(data));
+                            callback(data || []);
+                        },
+                    error:        function (xhr, status) 
+                        { 
+                            jQuery("#pageLoadStatus").text("HTTP GET "+urlval+" failed: "+status+"; HTTP status: "+xhr.status+" "+xhr.statusText);
+                            jQuery("#pageLoadStatus").addClass('error');
+                        },
+                    cache:        false
+              });
+}
