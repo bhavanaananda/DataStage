e6c4e00a279b3eac11a96ff54774db31f868ec46
bhavana <none@none>
2011-04-28 11:49:30 +0100

    Admin UI (restful web services - work in progress) - recified the ListAdmiralUsershandler webservice

--- src/AdminUIHandler/services/ListAdmiralUsersHandler.py
+++ src/AdminUIHandler/services/ListAdmiralUsersHandler.py
@@ -12,20 +12,15 @@ if __name__ == "__main__":
 
 class PrintStatement:
     def GET(self):
-
-        #print '<html><body><b>Hello World</b></body></html>'
-        
-        #if  os.environ.has_key("REMOTE_USER"):
-        #   uname =  os.environ['REMOTE_USER']
-            uname = "bhavana"
+        if  os.environ.has_key("REMOTE_USER"):
+            uname =  os.environ['REMOTE_USER']
+            #uname = "bhavana"
             logger.debug("Remote user = " + repr(uname))
-            #dirPath = "/mnt/lv-admiral-data/home"
-            dirPath = "/home"
+            dirPath = "/mnt/lv-admiral-data/home"
+            #dirPath = "/home"
             accesspath = "/usr/local/sbin/listAdmiralUsers.sh" + " " + uname + " " + dirPath
             logger.debug("accesspath = " + repr(accesspath))
-            #accessStatus = os.system(accesspath)
-            #return accessStatus
-            cmdOutput = subprocess.Popen([accesspath], stdout=subprocess.PIPE)
-            #logger.debug("accessStatus = " + repr(cmdOutput))
+            cmdOutput = subprocess.Popen(accesspath, shell=True, stdout=subprocess.PIPE)
+            logger.debug("cmdOutput = " + repr(cmdOutput))
             return cmdOutput.stdout.read()
             #return "Hello, world!"
