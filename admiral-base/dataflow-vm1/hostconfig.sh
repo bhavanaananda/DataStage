5ac6c718ad8381edb829f26d7a68db246057938e
bhavana <bhavana.kaushik@gmail.com>
2011-08-04 15:25:50 +0100

    Created hostconfig for oerc and bodleian machines

---
+++ admiral-base/dataflow-vm1/hostconfig.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Configuration parameters for host zoo-admiral-ibrg
+#
+# (See also: admiralconfig.sh)
+#
+
+RESEARCHGROUPNAME="DataStage"
+
+ADMIRALDOMAINDC="oerc ox ac uk"
+ADMIRALHOSTNAME="dataflow-vm1"
+
+# Samba/Windows workgroup name for research group files
+WORKGROUP="SHOTTON"
+
+# Databank host and silo names
+
+DATABANKHOST="databank.ora.ox.ac.uk"
+DATABANKSILO="admiral"
+
+# End.
+
---
+++ admiral-base/datastage-test/hostconfig.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Configuration parameters for host zoo-admiral-ibrg
+#
+# (See also: admiralconfig.sh)
+#
+
+RESEARCHGROUPNAME="DataStage"
+
+ADMIRALDOMAINDC="bodleian ox ac uk"
+ADMIRALHOSTNAME="datastage-test"
+
+# Samba/Windows workgroup name for research group files
+WORKGROUP="SHOTTON"
+
+# Databank host and silo names
+
+DATABANKHOST="databank.ora.ox.ac.uk"
+DATABANKSILO="admiral"
+
+# End.
+
